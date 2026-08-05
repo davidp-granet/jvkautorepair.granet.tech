c7e4909f550fcdd62b71723564d5898d61239978
davep <davep@granet.tech>
2026-08-05 01:34:35 -0400

    Navigating to the top no longer reloads the page

--- script.js
+++ script.js
@@ -192,3 +192,14 @@ window.addEventListener('DOMContentLoaded', () => {
     openSeasonalModal();
   }
 });
+
+// End Seasonal modal
+
+const topLinks = document.querySelectorAll('a[href="/"]');
+topLinks.forEach(elem => {
+  elem.addEventListener('click', function (event) {
+    event.preventDefault();
+    scroll(0, 0);
+    history.replaceState(null, null, '/');
+  });
+});
